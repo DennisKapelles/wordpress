2dcd74bd384ce0f1a7ca2f520fd7f86123eee2e8
Dionysios Kapelles <denniskapelles5@gmail.com>
2025-07-08 18:36:31 +0300

    Implementation of e-shop

---
+++ wordpress_project/wp-config.php
@@ -0,0 +1,95 @@
+<?php
+
+// BEGIN iThemes Security - Do not modify or remove this line
+// iThemes Security Config Details: 2
+define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settings > WordPress Tweaks > File Editor
+// END iThemes Security - Do not modify or remove this line
+
+/**
+ * Οι βασικές ρυθμίσεις για to WordPress
+ *
+ * Το wp-config.php χρησιμοποιείται από την δέσμη ενεργειών κατά την
+ * διαδικασία εγκατάστασης. Δεν χρειάζεται να χρησιμοποιήσετε τον ιστότοπο, μπορείτε
+ * να αντιγράψετε αυτό το αρχείο ως "wp-config.php" και να συμπληρώσετε τις παραμέτρους.
+ *
+ * Αυτό το αρχείο περιέχει τις ακόλουθες ρυθμίσεις:
+ *
+ * * MySQL ρυθμίσεις
+ * * Κλειδιά ασφαλείας
+ * * Πρόθεμα πινάκων βάσης δεδομένων
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL ρυθμίσεις - Μπορείτε να λάβετε αυτές τις πληροφορίες από τον φιλοξενητή σας ** //
+/** Το όνομα της βάσης δεδομένων του WordPress */
+define( 'DB_NAME', 'e_pix_project01' );
+
+/** Ψευδώνυμο χρήσης MySQL */
+define( 'DB_USER', 'root' );
+
+/** Συνθηματικό βάσης δεδομένων MySQL */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Charset της βάσηςη δεδομένων που θα χρησιμοποιηθεί στην δημιουργία των πινάκων. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** Τύπος Collate της βάσης δεδομένων. Μην το αλλάζετε αν έχετε αμφιβολίες. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Μοναδικά κλειδιά πιστοποίησηςη και Salts.
+ *
+ * Αλλάξτε τα σε διαφορετικά μοναδικές φράσεις!
+ * Μπορείτε να δημιουργήσετε χρησιμοποιώντας {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * Μπορείτε να τα αλλάξετε οποτεδήποτε για να ακυρώσετε τα υπάρχοντα cookies. Θα υποχρεώσει όλους χρήστες να επανασυνδεθούν.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '2uV0ld!_tEiI`+G9:$|TcE7$zE_nG{5c=Hn S=-LXqki_HQ0(k}$C>y=O]Rx1rY[' );
+define( 'SECURE_AUTH_KEY',  '4_-PF24X>&ly4|d!g`69|<fGX5Ac6[_oz&HQmD_iwkuqD)16D4w;vg&Hlg1YZM|`' );
+define( 'LOGGED_IN_KEY',    'HofC_(sYw2^-tO|.#u~rw6?9Pq;C3X=,8B{na%ln-o=(5_(A(%`<S$.+wh32??Y9' );
+define( 'NONCE_KEY',        'yTQ_(-Pk;8~XY&DdHES:j+[1Zj?oT_2^y)1M3QVF_W0tVz6H8,6W?S^ouTc]bYkd' );
+define( 'AUTH_SALT',        'S}c$P[yY(4bFT;[,xWSeIF?8J<pXzk?`/(>,}4eF|1+TC5KQn[hnkv`2Wk7S0-v6' );
+define( 'SECURE_AUTH_SALT', 'UD!SAR?{te*2jV[-yEGHKAB?y%(i)#]]w;^5B*uJvo|#gtla0626VBXnF5;3Ig+y' );
+define( 'LOGGED_IN_SALT',   '#d*y!?Q85UQ<yd;1){Oxwl0d2NV(h=tRsk5I [`Ap~@g>ydr-K:Y>hk9< _[jk`K' );
+define( 'NONCE_SALT',       'k!>{Lw]EsE^(szt1cTV) d<3OR<~7M[Ist9(0Ipsn5 bywF@qem(zoKRKHA,}$2E' );
+
+/**#@-*/
+
+/**
+ * Πρόθεμα Πίνακα Βάσης Δεδομένων του WordPress.
+ *
+ * Μπορείτε να έχετε πολλαπλές εγκαταστάσεις σε μια βάση δεδομένων αν δώσετε σε κάθε μία
+ * ένα μοναδικό πρόθεμα. Μόνο αριθμοί, γράμματα και κάτω παύλα παρακαλούμε!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Για προγραμματιστές: Κατάσταση Απασφαλμάτωσης WordPress (Debugging Mode).
+ *
+ * Αλλάξτε το σε true για να ενεργοποιήσετε την εμφάνισης ειδοποιήσεων για την διαδικασία ανάπτυξης.
+ * Η χρήση WP_DEBUG προτείνεται για τους δημιουργούς προσθέτων και θεμάτων
+ * στο περιβάλλον ανάπτυξης τους.
+ *
+ * Για πληροφορίες για άλλες σταθερές που μπορούν να χρησιμοποιηθούν για απασφαλμάτωση,
+ * επισκευθείτε το Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* Αυτό είναι όλο, σταματήστε γράφετε! Χαρούμενο blogging. */
+
+/** Η απόλυτη διαδρομή τον κατάλογο του WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Ορίζει τις μεταβλητές και τα περιλαμβανόμενα αρχεία WordPress. */
+require_once(ABSPATH . 'wp-settings.php');
